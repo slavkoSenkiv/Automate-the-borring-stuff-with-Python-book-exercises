a771692a25dda16c9a8f34ffc7dd7dedcb4ef7ba
slavkoSenkiv <slavko.senkiv@gmail.com>
2021-01-28 12:10:47 +0200

    upd

---
+++ book automate the borring stuff with python/python basics/2 flow control/Stone Scissors Papper game-exrcise (my version).py
@@ -0,0 +1,74 @@
+import random
+
+playerWins = 0
+computerWins = 0
+ties = 0
+moves = 0
+
+while True:
+    print('enter your figure')
+    player = input()
+    if player == 'r' or player == 's' or player == 'p':
+        computerGuess = random.randint(0, 2)
+        if computerGuess == 0:
+            computer = 'r'
+        if computerGuess == 1:
+            computer = 's'
+        if computerGuess == 2:
+            computer = 'p'
+        print('vs ' + computer)
+
+        # game logic here
+        if player == 'r' and computer == 'r':
+            ties += 1
+            moves += 1
+            print('C: ' + str(computerWins) + '. P: ' + str(playerWins) + '. T: ' + str(ties))
+        if player == 's' and computer == 's':
+            ties += 1
+            moves += 1
+            print('C: ' + str(computerWins) + '. P: ' + str(playerWins) + '. T: ' + str(ties))
+        if player == 'p' and computer == 'p':
+            ties += 1
+            moves += 1
+            print('C: ' + str(computerWins) + '. P: ' + str(playerWins) + '. T: ' + str(ties))
+
+        if player == 'r' and computer == 's':
+            playerWins += 1
+            moves += 1
+            print('C: ' + str(computerWins) + '. P: ' + str(playerWins) + '. T: ' + str(ties))
+        if player == 'r' and computer == 'p':
+            computerWins += 1
+            moves += 1
+            print('C: ' + str(computerWins) + '. P: ' + str(playerWins) + '. T: ' + str(ties))
+
+        if player == 's' and computer == 'p':
+            playerWins += 1
+            moves += 1
+            print('C: ' + str(computerWins) + '. P: ' + str(playerWins) + '. T: ' + str(ties))
+        if player == 's' and computer == 'r':
+            computerWins += 1
+            moves += 1
+            print('C: ' + str(computerWins) + '. P: ' + str(playerWins) + '. T: ' + str(ties))
+
+        if player == 'p' and computer == 's':
+            computerWins += 1
+            moves += 1
+            print('C: ' + str(computerWins) + '. P: ' + str(playerWins) + '. T: ' + str(ties))
+        if player == 'p' and computer == 's':
+            playerWins += 1
+            moves += 1
+            print('C: ' + str(computerWins) + '. P: ' + str(playerWins) + '. T: ' + str(ties))
+
+    else:
+        print('enter the right figure')
+
+    if playerWins == 3 or computerWins == 3:
+        if playerWins > computerWins:
+            print('Player won in ' + str(moves) + ' moves')
+            break
+        if computerWins > playerWins:
+            print('Computer won in ' + str(moves) + ' moves')
+            break
+
+
+
