a451030c01e82ed841a657337364088b955c39ec
slavkoSenkiv <slavko.senkiv@gmail.com>
2021-03-07 16:04:32 +0200

    upd

---
+++ book_automate_the_borring_stuff_with_python/part 2 Automating Tasks/9 reading and wring files/9 reading and writing files.py
@@ -0,0 +1,20 @@
+# Files and File Paths
+# Backslash on Windows and Forward Slash on macOS and Linux
+
+
+
+from pathlib import Path
+
+"""x = Path('bacon', 'bacon', 'eggs')
+print(str(x))
+
+from pathlib import Path
+
+myFiles = ['accounts.txt', 'details.csv', 'invite.docx']
+for filename in myFiles:
+    print(Path(r'C:\ Users\Al', filename))"""
+
+
+# Using the / Operator to Join Paths
+
+print(Path('код', 'atbs', 'part2', '9 reading files'))
