0151003830e7c8a295312a54b76946543fae9dda
slavkoSenkiv <slavko.senkiv@gmail.com>
2021-03-28 12:31:58 +0300

    upd

--- book_automate_the_borring_stuff_with_python/part 2 Automating Tasks/9 reading and wring files/Mad Libs Project/mad libs.py
+++ book_automate_the_borring_stuff_with_python/part 2 Automating Tasks/9 reading and wring files/Mad Libs Project/mad libs.py
@@ -3,7 +3,7 @@ import re
 
 # TODO: + asks what file u what to modify and opens that file
 # TODO: + print actual content of the file
-# TODO: with regex finds if there are key-words
+# TODO: + with regex finds if there are key-words
 # TODO: if script find any keys in file it promt u to input your value for every keyword it found
 # TODO: sript replace keys with your imputed value
 # TODO: after all found keys are replaced it says it
@@ -15,13 +15,16 @@ fileContent = file.read()
 print(fileContent)
 
 adjectiveRegex = re.compile(r'ADJECTIVE')
-mo = adjectiveRegex.search(fileContent)
-print(f'we found: {mo.group()}')
-mo.group() = 'NEW VALUE'
-print(fileContent)
+matchAdjective = adjectiveRegex.search(fileContent)
+print(f'we found: {matchAdjective.group()}')
+if matchAdjective:
+    print('do smth')
 
+nounRegex = re.compile(r'NOUN')
+matchNoun = nounRegex.search(fileContent)
+print(f'we found: {matchNoun.group()}')
 
-# nounRegex
-# adverbRegex
-# verbRegex
+verbRegex = re.compile(r'VERB')
+matchVerb = verbRegex.search(fileContent)
+print(f'we found: {matchVerb.group()}')
 
