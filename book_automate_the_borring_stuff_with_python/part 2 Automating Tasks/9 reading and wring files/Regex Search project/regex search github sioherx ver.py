93e8e54eb38b324fd51d9f355d57d57b2c24d6d1
slavkoSenkiv <slavko.senkiv@gmail.com>
2021-03-31 10:18:49 +0300

    usr

--- book_automate_the_borring_stuff_with_python/part 2 Automating Tasks/9 reading and wring files/Regex Search project/regex search github sioherx ver.py
+++ book_automate_the_borring_stuff_with_python/part 2 Automating Tasks/9 reading and wring files/Regex Search project/regex search github sioherx ver.py
@@ -1,16 +1,27 @@
+#! python 3.5
+# regex search.py - opens all .txt files in a folder and searches for any line
+# that matches a user-supplied regular expression
+
 import os, re
 
-def search (regex, text):
+def search_function(regex, content):                # this function print all found matches in the content
     search_regex = re.compile(regex, re.I)
-    result = search_regex.findall(txt)
-    print(result)
+    found_matches = search_regex.findall(content)
+    print(found_matches)
 
 while True:
-    dirs = input('enter folder u need to research: ')
-    if os.path.exists(dirs) == True:
-        print('this folder exists')
+    directory = input('Enter the absolute path to the folder, which files  you want to search for: \n')
+    if os.path.exists(directory) == True:
+        print('This folder exists')
         break
 
-    user_search = input('enter your regex: ')
+users_regex = input('Enter the regular expression: \n')
 
-    folder 
+folders_files = os.listdir(directory)
+
+for file in folders_files:
+    if file.endswith('.txt'):
+        print(os.path.join(directory, file))
+        txtfile = open(os.path.join(directory, file), 'r+')
+        file_content = txtfile.read()
+        search_function(users_regex, file_content)
