223ef373dd5fcbba036ab7fa8a58eb180f1b260f
slavkoSenkiv <slavko.senkiv@gmail.com>
2021-04-06 09:46:54 +0300

    upd

--- book_automate_the_borring_stuff_with_python/part 2 Automating Tasks/10 organizing files/10 organizing files.py
+++ book_automate_the_borring_stuff_with_python/part 2 Automating Tasks/10 organizing files/10 organizing files.py
@@ -46,7 +46,6 @@ bacon_file = open('bacon.txt', 'a') # creates the file
 bacon_file.write('Bacon is not a vegetable')
 bacon_file.close()
 send2trash.send2trash('bacon.txt')
-"""
 
 # Walking a Directory Tree
 import os
@@ -62,3 +61,46 @@ for folderName, subFolders, fileNames in os.walk(p):
         print('FILE INSIDE ' + folderName + ': ' + filename)
 
     print('')
+
+# Compressing Files with the zipfile Module
+import zipfile, os
+from pathlib import Path
+
+p = Path.home()
+example_zip = zipfile.ZipFile(p / 'abc.zip')
+print(example_zip.namelist())
+spam_info = example_zip.getinfo('1 level doc.txt')
+print(spam_info.file_size)
+print(spam_info.compress_size)
+print(f'compressed file is {round(spam_info.file_size / spam_info.compress_size, 2)}X smaller')
+
+
+# Extracting from ZIP Files
+import zipfile, os
+from pathlib import Path
+p = Path.home()
+abcZip = zipfile.ZipFile(p / 'abc.zip')
+abcZip.extractall(Path.home())
+abcZip.close()
+
+import zipfile
+from pathlib import Path
+
+p = Path.home()
+abcZip = zipfile.ZipFile(p / 'abc.zip')
+abcZip.extract('1 level doc.txt')
+abcZip.close()
+
+# Creating and Adding to ZIP Files
+
+import zipfile
+newZip = zipfile.ZipFile('new.zip', 'w')
+newZip.write('newSpam.txt', compress_type=zipfile.ZIP_DEFLATED)
+newZip.close()
+"""
+
+
+
+
+
+
