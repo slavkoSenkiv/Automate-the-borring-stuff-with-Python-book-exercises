3b321765e31b81753bc0ce71982d10610d895fa9
slavkoSenkiv <slavko.senkiv@gmail.com>
2021-04-11 10:03:17 +0300

    upd

---
+++ book_automate_the_borring_stuff_with_python/part 2 Automating Tasks/10 organizing files/Selective Copy Project/test.py
@@ -0,0 +1,14 @@
+import os, shutil
+
+def selectiveCopy(startFolder, endFolder):
+
+    for folderName, subFolders, fileNames in os.walk(startFolder):
+        print(f'Copying files from {folderName}')
+        for fileName in fileNames:
+            if fileName.endswith('.txt'):
+                print(f'Copying {fileName} from {folderName} to {endFolder}..')
+                # shutil.copy(os.path.join(startFolder, fileName), endFolder)
+
+a = 'C:\\Users\\Yaroslav\\a'
+b = 'C:\\Users\\Yaroslav\\b'
+selectiveCopy(a, b)
