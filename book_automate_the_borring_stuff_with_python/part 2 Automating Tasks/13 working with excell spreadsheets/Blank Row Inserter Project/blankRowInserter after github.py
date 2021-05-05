61656d5b340572a580247a92f8a9fb96eae54d61
slavkoSenkiv <slavko.senkiv@gmail.com>
2021-05-05 15:31:08 +0300

    upd

--- book_automate_the_borring_stuff_with_python/part 2 Automating Tasks/13 working with excell spreadsheets/Blank Row Inserter Project/blankRowInserter after github.py
+++ book_automate_the_borring_stuff_with_python/part 2 Automating Tasks/13 working with excell spreadsheets/Blank Row Inserter Project/blankRowInserter after github.py
@@ -10,6 +10,11 @@ inputSheet = inputWb.active
 outputWb = openpyxl.Workbook()
 outputSheet = outputWb.active
 
-
+for rowNum in range(1, inputSheet.max_row + 1):
+    for colNum in range(1, inputSheet.max_column + 1):
+        if rowNum <= insertAfter:
+            outputSheet.cell(row=rowNum, column=colNum).value = inputSheet.cell(row=rowNum, column=colNum).value
+        else:
+            outputSheet.cell(row=rowNum + howManyInsert, column=colNum).value = inputSheet.cell(row=rowNum, column=colNum).value
 
 outputWb.save(f'new_{workBookName}_afterGitHub.xlsx')
