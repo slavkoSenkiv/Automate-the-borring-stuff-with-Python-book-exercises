0d20a57e58fe7cd2564119b435dc174c7aacf50f
slavkoSenkiv <slavko.senkiv@gmail.com>
2021-06-01 20:24:02 +0300

    upd

---
+++ book_automate_the_borring_stuff_with_python/part 2 Automating Tasks/12 web scrapping/mapIt.py with the webbrowser Module Project/mapit before read book.py
@@ -0,0 +1,16 @@
+#! python3
+# mapIt.py - Launches a map in the browser using an address from the
+# command line or clipboard.
+
+import webbrowser, sys, pyperclip
+
+if len(sys.argv) > 1:
+    # get address from cmd
+    address = sys.argv[1:]
+    urlSuffix = ''
+    for words in address:
+        if words == address[-1]:
+            urlSuffix += words
+        else:
+            urlSuffix += words + '+'
+    webbrowser.open('https://www.google.com/maps/place/' + urlSuffix)
